ccadeb25cd44fa51cf6dc8638e2a7c661df32315
Casey Occhialini <1508707+littlespex@users.noreply.github.com>
2024-06-18 17:41:22 -0700

    feat: add svelte example app

---
+++ examples/svelte/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Player from './Player.svelte';
+</script>
+
+<section class="player">
+  <Player />
+</section>
+
+<style>
+  .player {
+    background-color: black;
+  }
+</style>
---
+++ examples/svelte/src/routes/Player.svelte
@@ -0,0 +1,45 @@
+<script type="ts">
+  import { onMount } from 'svelte';
+
+  onMount(async () => {
+    await import('media-chrome');
+  });
+</script>
+
+<div>
+  <media-controller>
+    <video
+      playsinline
+      slot="media"
+      src="https://stream.mux.com/A3VXy02VoUinw01pwyomEO3bHnG4P32xzV7u1j1FSzjNg/high.mp4"
+    ></video>
+    <media-poster-image
+      slot="poster"
+      src="https://image.mux.com/A3VXy02VoUinw01pwyomEO3bHnG4P32xzV7u1j1FSzjNg/thumbnail.jpg"
+      placeholdersrc="data:image/jpeg;base64,/9j/2wBDABQODxIPDRQSEBIXFRQYHjIhHhwcHj0sLiQySUBMS0dARkVQWnNiUFVtVkVGZIhlbXd7gYKBTmCNl4x9lnN+gXz/2wBDARUXFx4aHjshITt8U0ZTfHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHz/wAARCAAUADADASIAAhEBAxEB/8QAGAAAAwEBAAAAAAAAAAAAAAAAAAECBAP/xAAdEAEBAAEEAwAAAAAAAAAAAAAAARECAxITFCFR/8QAGQEAAwADAAAAAAAAAAAAAAAAAAEDAgQF/8QAGBEBAQEBAQAAAAAAAAAAAAAAAAETERL/2gAMAwEAAhEDEQA/ANeC4ldyI1b2EtIzzrrIqYZLvl5FGkGdbfQzGPvo76WsPxXLlfqbaA5va2iVJADgPELACsD/2Q=="
+    ></media-poster-image>
+    <media-loading-indicator slot="centered-chrome" noautohide
+    ></media-loading-indicator>
+    <media-control-bar>
+      <media-play-button></media-play-button>
+      <media-mute-button></media-mute-button>
+      <media-volume-range></media-volume-range>
+      <media-time-display></media-time-display>
+      <media-time-range></media-time-range>
+      <media-duration-display></media-duration-display>
+      <media-playback-rate-button></media-playback-rate-button>
+      <media-fullscreen-button></media-fullscreen-button>
+    </media-control-bar>
+  </media-controller>
+</div>
+
+<style>
+  media-controller {
+    display: block; /* expands the container if preload=none */
+    aspect-ratio: 2.4; /* set container aspect ratio if preload=none */
+  }
+
+  video {
+    width: 100%; /* prevents video to expand beyond its container */
+  }
+</style>
